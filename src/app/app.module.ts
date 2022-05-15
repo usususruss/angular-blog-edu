e4f1d172defcea99d5d56d83d9644cfc1f00406f
Yuri Sergeev <ususus_russ@mail.ru>
2022-05-15 19:16:56 +0300

    Registred RU locale.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule, Provider } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PostPageComponent } from './post-page/post-page.component';
-import { AuthInterceptor } from './shared/auth.interceptor';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import { PostComponent } from './shared/components/post/post.component';
-import { SharedModule } from './shared/shared.module';
+import { registerLocaleData } from '@angular/common'
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
+import ruLocale from '@angular/common/locales/ru'
+import { NgModule, Provider } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomePageComponent } from './home-page/home-page.component'
+import { PostPageComponent } from './post-page/post-page.component'
+import { AuthInterceptor } from './shared/auth.interceptor'
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component'
+import { PostComponent } from './shared/components/post/post.component'
+import { SharedModule } from './shared/shared.module'
 
+
+registerLocaleData(ruLocale, 'ru')
 
 const INTERCEPTOR_PROVIDER: Provider = {
     provide: HTTP_INTERCEPTORS,
